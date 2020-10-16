8e8b61cc02e3c1c33e100cf9d797d2075ef8b6fd
TiannaDeSpain <u663140950@byui.edu>
2020-10-16 15:29:33 -0700

    week 05 work intial commit

--- js/lastModified.js
+++ js/lastModified.js
@@ -1,2 +1,13 @@
 document.getElementById('lastmodified').innerHTML = `Last Updated: ${document.lastModified}`;
 document.getElementById('year').innerHTML = `&copy; ${new Date().getFullYear()}`;
+
+
+let options = {
+    weekday: "long",
+    day: "numeric", 
+    month: "long",
+    year: "numeric"
+};
+
+document.getElementById("currentDate").textContent = new Date().toLocaleDateString("en-GB", options);
+document.getElementById("currentYear").innerHTML = "&copy; " + new Date().getFullYear();
