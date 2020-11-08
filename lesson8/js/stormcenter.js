fb519d2605436b491bc0f9e6c4f7d9396716dea4
TiannaDeSpain <u663140950@byui.edu>
2020-11-07 19:43:48 -0700

    fixed anme of thanks

--- lesson8/js/stormcenter.js
+++ lesson8/js/stormcenter.js
@@ -1,8 +1,3 @@
 function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
-function ThankYou(){
-    window.open(
-        'thankyou.html', '_self'
-    )
-}
