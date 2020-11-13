26ccbd2b5955b5afd1d19d4d8b0456cdecc2e5e6
TiannaDeSpain <u663140950@byui.edu>
2020-11-12 17:03:45 -0700

    prophets load

---
+++ lesson9/js/prophets.js
@@ -0,0 +1,33 @@
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const prophets = jsonObject['prophets']; 
+    for (let i = 0; i < prophets.length; i++ ) {
+        let card = document.createElement('section');
+        let h2 = document.createElement('h2');
+        let birthDate = document.createElement('p')
+        let birthPlace = document.createElement('p')
+        let portrait = document.createElement('img')
+        let portraitAlt = prophets[i].name + " " + prophets[i].lastname + " : " + prophets[i].order;
+
+        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        birthDate.textContent = "Date of Birth: " + prophets[i].birthdate;
+        birthPlace.textContent = "Birth place: " + prophets[i].birthplace;
+        portrait.setAttribute('src', prophets[i].imageurl);
+        portrait.setAttribute('alt', portraitAlt);
+
+        card.appendChild(h2);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(portrait);
+
+        document.querySelector('div.cards').appendChild(card);
+    }
+});
+
+  
+  
